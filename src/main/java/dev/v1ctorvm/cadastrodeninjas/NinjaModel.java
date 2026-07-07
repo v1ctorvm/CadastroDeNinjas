fb24babd94120909ddf934aaf8cdf7d45339e311
v1ctorvm <vitinmtxs@gmail.com>
2026-07-07 20:32:55 -0300

    Cria uma entidade no banco de dados apartir do NinjaModel

--- src/main/java/dev/v1ctorvm/cadastrodeninjas/NinjaModel.java
+++ src/main/java/dev/v1ctorvm/cadastrodeninjas/NinjaModel.java
@@ -1,4 +1,56 @@
 package dev.v1ctorvm.cadastrodeninjas;
 
-public class Ninja {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
