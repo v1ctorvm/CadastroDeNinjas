158db3c90959f18121d8e38002f95820d8ad485c
v1ctorvm <vitinmtxs@gmail.com>
2026-07-09 20:59:39 -0300

    feat: add Lombok in classes

--- src/main/java/dev/v1ctorvm/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/dev/v1ctorvm/cadastrodeninjas/Missoes/MissoesModel.java
@@ -2,11 +2,19 @@ package dev.v1ctorvm.cadastrodeninjas.Missoes;
 
 import dev.v1ctorvm.cadastrodeninjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "db_missoes")
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class MissoesModel {
 
     @Id
@@ -18,45 +26,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes") // uma missao pode ter varios ninjas
     private List<NinjaModel> ninjas;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nome, String dificuldade, List<NinjaModel> ninjas) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninjas = ninjas;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
--- src/main/java/dev/v1ctorvm/cadastrodeninjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/v1ctorvm/cadastrodeninjas/Ninjas/NinjaModel.java
@@ -2,11 +2,15 @@ package dev.v1ctorvm.cadastrodeninjas.Ninjas;
 
 import dev.v1ctorvm.cadastrodeninjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro")
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class NinjaModel {
 
     @Id
@@ -20,45 +24,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreign Key - Chave estrangeira
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
