d9e63662fa06497518f255e3cdda5dba85d805d1
v1ctorvm <vitinmtxs@gmail.com>
2026-07-12 02:08:44 -0300

    build: connect H2 DB to my Spring Application

--- src/main/java/dev/v1ctorvm/cadastrodeninjas/Missoes/MissoesModel.java
+++ src/main/java/dev/v1ctorvm/cadastrodeninjas/Missoes/MissoesModel.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
-@Table(name = "db_missoes")
+@Table(name = "tb_missoes")
 
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/dev/v1ctorvm/cadastrodeninjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/v1ctorvm/cadastrodeninjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,6 @@
 package dev.v1ctorvm.cadastrodeninjas.Ninjas;
 
-public class NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel,Long> {
 }
