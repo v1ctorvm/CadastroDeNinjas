517f45ac0d0f7ebe822bc5b1a08d8be7f7ad38ed
v1ctorvm <vitinmtxs@gmail.com>
2026-07-26 14:25:07 -0300

    chore: inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos de entidade x DTO

--- src/main/java/dev/v1ctorvm/cadastrodeninjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/v1ctorvm/cadastrodeninjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,22 @@
 package dev.v1ctorvm.cadastrodeninjas.Ninjas;
 
+import dev.v1ctorvm.cadastrodeninjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private String rank;
+    private MissoesModel missoes;
+
+
 }
